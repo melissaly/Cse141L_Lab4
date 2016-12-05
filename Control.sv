eb343fcb5e941c1d6569508229a56217a4f5ccc8
sawandeepk <sawandeep.kaur@gmail.com>
2016-12-04 17:28:05 -0800

    Add files via upload

--- Control.sv
+++ Control.sv
@@ -1,52 +1,145 @@
-
-module Control( //in: opcode; out: ALUOP, BRANCH, PUSH_TOP, PUSH_PEN, POP_TOP, POP_PEN, WRITE_REG, WRITE_EN, HALT
+module Control(
 	input		[3:0]	OPCODE,
 	output logic 	[2:0]	ALUOP,
 	output logic		BRANCH,
+	output logic	[1:0]	PUSH_SEL,
 	output logic		PUSH_TOP,
 	output logic		PUSH_PEN,
 	output logic		POP_TOP,
 	output logic		POP_PEN,
-	output logic		WRITE_REG,
+	output logic	[1:0]	WRITE_REG,
 	output logic		WRITE_EN,
+	output logic		WRITE_MEM,
 	output logic		HALT
 	);
 
 	always_comb begin
 	case(OPCODE)
-		3:	begin
+		0:	begin
 			 ALUOP = 0;
 			 BRANCH = 0;
-			 PUSH_TOP = 0;
+			 PUSH_SEL = 0;
+			 PUSH_TOP = 1;
 			 PUSH_PEN = 0;
-			 POP_TOP = 1;
-			 POP_PEN = 1;
-			 WRITE_REG = 1;
-			 WRITE_EN = 1;
+			 POP_TOP = 0;
+			 POP_PEN = 0;
+			 WRITE_REG = 0;
+			 WRITE_EN = 0;
+			 WRITE_MEM = 0;
 			 HALT = 0;
 		end
 
-		7:	begin
-			 ALUOP = 1;
-			 BRANCH = 1;
+		1:	begin
+			 ALUOP = 0;
+			 BRANCH = 0;
+			 PUSH_SEL = 0;
 			 PUSH_TOP = 0;
 			 PUSH_PEN = 0;
 			 POP_TOP = 0;
 			 POP_PEN = 0;
 			 WRITE_REG = 0;
 			 WRITE_EN = 0;
+			 WRITE_MEM = 1;
 			 HALT = 0;
 		end
 
-		8:	begin
-			 ALUOP = 2;
+		2:	begin
+			 ALUOP = 0;
 			 BRANCH = 0;
+			 PUSH_SEL = 2;
+			 PUSH_TOP = 1;
+			 PUSH_PEN = 0;
+			 POP_TOP = 0;
+			 POP_PEN = 0;
+			 WRITE_REG = 0;
+			 WRITE_EN = 0;
+			 WRITE_MEM = 0;
+			 HALT = 0;
+		end
+
+
+		3:	begin
+			 ALUOP = 0;
+			 BRANCH = 0;
+			 PUSH_SEL = 0;
+			 PUSH_TOP = 0;
+			 PUSH_PEN = 0;
+			 POP_TOP = 1;
+			 POP_PEN = 1;
+			 WRITE_REG = 2;
+			 WRITE_EN = 1;
+			 WRITE_MEM = 0;
+			 HALT = 0;
+		end
+
+		4:	begin
+			 ALUOP = 0;
+			 BRANCH = 0;
+			 PUSH_SEL = 0;
+			 PUSH_TOP = 1;
+			 PUSH_PEN = 0;
+			 POP_TOP = 0;
+			 POP_PEN = 0;
+			 WRITE_REG = 0;
+			 WRITE_EN = 0;
+			 WRITE_MEM = 0;
+			 HALT = 0;
+		end
+
+		5:	begin
+			 ALUOP = 0;
+			 BRANCH = 0;
+			 PUSH_SEL = 1;
 			 PUSH_TOP = 1;
 			 PUSH_PEN = 0;
 			 POP_TOP = 0;
 			 POP_PEN = 0;
 			 WRITE_REG = 1;
 			 WRITE_EN = 1;
+			 WRITE_MEM = 0;
+			 HALT = 0;
+		end
+
+		6:	begin
+			 ALUOP = 0;
+			 BRANCH = 0;
+			 PUSH_SEL = 0;
+			 PUSH_TOP = 0;
+			 PUSH_PEN = 0;
+			 POP_TOP = 1;
+			 POP_PEN = 1;
+			 WRITE_REG = 0;
+			 WRITE_EN = 0;
+			 WRITE_MEM = 0;
+			 HALT = 0;
+		end
+
+
+		7:	begin
+			 ALUOP = 1;
+			 BRANCH = 1;
+			 PUSH_SEL = 0;
+			 PUSH_TOP = 0;
+			 PUSH_PEN = 0;
+			 POP_TOP = 0;
+			 POP_PEN = 0;
+			 WRITE_REG = 0;
+			 WRITE_EN = 0;
+			 WRITE_MEM = 0;
+			 HALT = 0;
+		end
+
+		8:	begin
+			 ALUOP = 2;
+			 BRANCH = 0;
+			 PUSH_SEL = 3;
+			 PUSH_TOP = 1;
+			 PUSH_PEN = 0;
+			 POP_TOP = 0;
+			 POP_PEN = 0;
+			 WRITE_REG = 2;
+			 WRITE_EN = 1;
+			 WRITE_MEM = 0;
 			 HALT = 0;
 		end
 
@@ -54,12 +147,14 @@ module Control( //in: opcode; out: ALUOP, BRANCH, PUSH_TOP, PUSH_PEN, POP_TOP, P
 		9:	begin
 			 ALUOP = 3;
 			 BRANCH = 0;
+			 PUSH_SEL = 3;
 			 PUSH_TOP = 1;
 			 PUSH_PEN = 1;
 			 POP_TOP = 0;
 			 POP_PEN = 0;
 			 WRITE_REG = 0;
 			 WRITE_EN = 0;
+			 WRITE_MEM = 0;
 			 HALT = 0;
 		end
 
@@ -67,12 +162,14 @@ module Control( //in: opcode; out: ALUOP, BRANCH, PUSH_TOP, PUSH_PEN, POP_TOP, P
 		10:	begin
 			 ALUOP = 4;
 			 BRANCH = 0;
+			 PUSH_SEL = 0;
 			 PUSH_TOP = 0;
 			 PUSH_PEN = 0;
 			 POP_TOP = 1;
 			 POP_PEN = 0;
-			 WRITE_REG = 1;
+			 WRITE_REG = 2;
 			 WRITE_EN = 1;
+			 WRITE_MEM = 0;
 			 HALT = 0;
 		end
 
@@ -80,12 +177,14 @@ module Control( //in: opcode; out: ALUOP, BRANCH, PUSH_TOP, PUSH_PEN, POP_TOP, P
 		11:	begin
 			 ALUOP = 5;
 			 BRANCH = 1;
+			 PUSH_SEL = 0;
 			 PUSH_TOP = 0;
 			 PUSH_PEN = 0;
 			 POP_TOP = 0;
 			 POP_PEN = 0;
 			 WRITE_REG = 0;
 			 WRITE_EN = 0;
+			 WRITE_MEM = 0;
 			 HALT = 0;
 		end
 
@@ -93,12 +192,14 @@ module Control( //in: opcode; out: ALUOP, BRANCH, PUSH_TOP, PUSH_PEN, POP_TOP, P
 		12:	begin
 			 ALUOP = 6;
 			 BRANCH = 0;
+			 PUSH_SEL = 0;
 			 PUSH_TOP = 0;
 			 PUSH_PEN = 0;
 			 POP_TOP = 0;
 			 POP_PEN = 0;
-			 WRITE_REG = 1;
+			 WRITE_REG = 2;
 			 WRITE_EN = 1;
+			 WRITE_MEM = 0;
 			 HALT = 0;
 		end
 
@@ -106,15 +207,47 @@ module Control( //in: opcode; out: ALUOP, BRANCH, PUSH_TOP, PUSH_PEN, POP_TOP, P
 		13:	begin
 			 ALUOP = 7;
 			 BRANCH = 0;
+			 PUSH_SEL = 3;
 			 PUSH_TOP = 1;
 			 PUSH_PEN = 0;
 			 POP_TOP = 0;
 			 POP_PEN = 0;
-			 WRITE_REG = 1;
+			 WRITE_REG = 2;
 			 WRITE_EN = 1;
+			 WRITE_MEM = 0;
 			 HALT = 0;
 		end
+
+		14:	begin
+			 ALUOP = 0;
+			 BRANCH = 0;
+			 PUSH_SEL = 0;
+			 PUSH_TOP = 0;
+			 PUSH_PEN = 0;
+			 POP_TOP = 0;
+			 POP_PEN = 0;
+			 WRITE_REG = 0;
+			 WRITE_EN = 0;
+			 WRITE_MEM = 0;
+			 HALT = 1;
+		end
+		default:begin
+			 ALUOP = 0;
+			 BRANCH = 0;
+			 PUSH_SEL = 0;
+			 PUSH_TOP = 0;
+			 PUSH_PEN = 0;
+			 POP_TOP = 0;
+			 POP_PEN = 0;
+			 WRITE_REG = 0;
+			 WRITE_EN = 0;
+			 WRITE_MEM = 0;
+			 HALT = 0;
+		end
+
+
 	endcase
 	end
 endmodule
 		
+	
--- stack.sv
+++ stack.sv
@@ -1,5 +1,4 @@
-
-module stack #(parameter W = 8, D = 4) ( // in: clk, push_top, push_pen, pop_top, pop_pen, pushTop, pushPen; out: topVal, penVal
+module stack #(parameter W = 8, D = 5) (
 	input clk, push_top, push_pen, pop_top, pop_pen,
 	input [W-1:0] pushTop,
 	input [W-1:0] pushPen,
@@ -7,36 +6,36 @@ module stack #(parameter W = 8, D = 4) ( // in: clk, push_top, push_pen, pop_top
 	output logic [W-1:0] penVal
 	);
 
-	logic [W-1:0] stacks[2**D];
-	int stackPtr = 0;
-	
-	
-	always_ff @ (posedge clk) begin
-		if(push_pen)
-		  begin
-			stacks[stackPtr] <= pushPen;
-			penVal <= pushPen;
-			stackPtr <= stackPtr + 1;
-		  end
-	
-		if(push_top)
-		  begin
-			stacks[stackPtr] <= pushTop;
-			topVal <= pushTop;
-			stackPtr <= stackPtr + 1;
-		  end
-	
-		if(pop_top)
-		  begin
-			stackPtr <= stackPtr - 1;
-			topVal <= stacks[stackPtr];
-		  end
-	
-		if(pop_pen)
-		  begin
-			stackPtr <= stackPtr - 1;
-			penVal <= stacks[stackPtr];
-		  end
-	end
+logic [W-1:0] stacks[2**D];
+int stackPtr = 0;
+
+
+always_ff @ (posedge clk) begin
+	if(push_pen)
+	  begin
+		stacks[stackPtr] <= pushPen;
+		penVal <= pushPen;
+		stackPtr <= stackPtr + 1;
+	  end
+
+	if(push_top)
+	  begin
+		stacks[stackPtr] <= pushTop;
+		topVal <= pushTop;
+		stackPtr <= stackPtr + 1;
+	  end
+
+	if(pop_top)
+	  begin
+		stackPtr <= stackPtr - 1;
+		topVal <= stacks[stackPtr];
+	  end
+
+	if(pop_pen)
+	  begin
+		stackPtr <= stackPtr - 1;
+		penVal <= stacks[stackPtr];
+	  end
+end
 
 endmodule
